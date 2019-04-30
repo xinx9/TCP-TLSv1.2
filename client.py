22e7b0e6f7557555c091af21f3902ca6b8bd2585
Brandon Burke <bcburke93@gmail.com>
2019-04-30 23:45:58 +0000

    simple TLSv1.2

--- client.py
+++ client.py
@@ -1,11 +1,13 @@
 import socket
 import ssl, sys, os
+
 dmax = 1024
+
 if(len(sys.argv) != 2):
     print("Usage: python3 client.py <PORT NUMBER>")
     sys.exit(1)
-TCP_ServerAddress = ('localhost', int(sys.argv[1]))
 
+TCP_ServerAddress = ('localhost', int(sys.argv[1]))
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ctx = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 conn = ctx.wrap_socket(sock, server_side=False, server_hostname='localhost')
